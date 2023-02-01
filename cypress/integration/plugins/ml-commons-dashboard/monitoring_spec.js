1e3cbe65c1867b8cc780c1aa84bfb0b9e434223f
Lin Wang <wonglam@amazon.com>
2023-02-01 18:56:25 +0800

    --wip-- [skip ci]

--- cypress/integration/plugins/ml-commons-dashboard/monitoring_spec.js
+++ cypress/integration/plugins/ml-commons-dashboard/monitoring_spec.js
@@ -1,1 +1,25 @@
-describe('MLC Monitoring page', () => {});
+import { MLC_URL } from '../../../utils/constants';
+
+describe('MLC Monitoring page', () => {
+  let uploadedInfo;
+  before(() => {
+    uploadedInfo = cy.uploadModelByUrl();
+  });
+
+  after(() => {
+    if (uploadedInfo) {
+      cy.deleteMLCommonsModel(uploadedInfo.modelId);
+      cy.deleteMLCommonsTask(uploadedInfo.taskId);
+    }
+  });
+
+  it('should return to monitoring page when visit root', () => {
+    cy.visit(MLC_URL.ROOT);
+    cy.url().should('include', MLC_URL.MONITORING);
+  });
+  it('page elements should be exists', () => {
+    cy.visit(MLC_URL.MONITORING);
+    cy.get('h1').contains('Overview');
+    cy.get('h3').contains('Deployed models');
+  });
+});
--- cypress/utils/plugins/ml-commons-dashboards/constants.js
+++ cypress/utils/plugins/ml-commons-dashboards/constants.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BASE_PATH } from '../../base_constants';
+import { BASE_PATH, BACKEND_BASE_PATH } from '../../base_constants';
 
 /**
  *****************************
@@ -15,21 +15,15 @@ const BASE_MLC_PATH = BASE_PATH + '/app/mlCommonsPlugin';
 
 export const MLC_URL = {
   ROOT: BASE_MLC_PATH + '/',
-  MONITORING: BASE_AD_PATH + '/monitoring',
+  MONITORING: BASE_MLC_PATH + '/monitoring',
 };
 
 export const MLC_FIXTURE_BASE_PATH = 'plugins/ml-commons-dashboards/';
 
-export const MLC_API_PREFIX = '/_plugins/_ml';
-export const MLC_APP_API_BASE = '/api/ml-commons';
+export const MLC_API_BASE = `${BACKEND_BASE_PATH}/_plugins/_ml`;
 
 export const MLC_API = {
-  MODEL_BASE: `${MLC_API_PREFIX}/models`,
-  TRAIN_BASE: `${MLC_API_PREFIX}/_train`,
-  TASK_BASE: `${MLC_API_PREFIX}/tasks`,
-};
-
-export const MLC_APP_API = {
-  MODEL: `${MLC_APP_API_BASE}/model`,
-  MODEL_ALGORITHM: `${MLC_APP_API_BASE}/model-algorithm`,
+  MODEL_BASE: `${MLC_API_BASE}/models`,
+  MODEL_UPLOAD: `${MLC_API_BASE}/models/_upload`,
+  TASK_BASE: `${MLC_API_BASE}/tasks`,
 };
