d03085fe8ce1b24c10391ecb42dc4ef7e9ee30f9
Lin Wang <wonglam@amazon.com>
2024-01-16 16:22:16 +0800

    feat: remove before each and after each
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- cypress/integration/plugins/anomaly-detection-dashboards-plugin/create_detector_spec.js
+++ cypress/integration/plugins/anomaly-detection-dashboards-plugin/create_detector_spec.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { AD_FIXTURE_BASE_PATH, AD_URL } from '../../../utils/constants';
+import { AD_URL } from '../../../utils/constants';
 import { selectTopItemFromFilter } from '../../../utils/helpers';
 
 context('Create detector workflow', () => {
@@ -14,32 +14,32 @@ context('Create detector workflow', () => {
   const TEST_INDEX_NAME = 'sample-ad-index';
 
   // Index some sample data first
-  beforeEach(() => {
-    cy.deleteAllIndices();
-    cy.deleteADSystemIndices();
-    cy.fixture(AD_FIXTURE_BASE_PATH + 'sample_test_data.txt').then((data) => {
-      cy.request({
-        method: 'POST',
-        form: false,
-        url: 'api/console/proxy',
-        headers: {
-          'content-type': 'application/json;charset=UTF-8',
-          'osd-xsrf': true,
-        },
-        qs: {
-          path: `${TEST_INDEX_NAME}/_bulk`,
-          method: 'POST',
-        },
-        body: data,
-      });
-    });
-  });
+  // beforeEach(() => {
+  //   cy.deleteAllIndices();
+  //   cy.deleteADSystemIndices();
+  //   cy.fixture(AD_FIXTURE_BASE_PATH + 'sample_test_data.txt').then((data) => {
+  //     cy.request({
+  //       method: 'POST',
+  //       form: false,
+  //       url: 'api/console/proxy',
+  //       headers: {
+  //         'content-type': 'application/json;charset=UTF-8',
+  //         'osd-xsrf': true,
+  //       },
+  //       qs: {
+  //         path: `${TEST_INDEX_NAME}/_bulk`,
+  //         method: 'POST',
+  //       },
+  //       body: data,
+  //     });
+  //   });
+  // });
 
-  // Clean up created resources
-  afterEach(() => {
-    cy.deleteAllIndices();
-    cy.deleteADSystemIndices();
-  });
+  // // Clean up created resources
+  // afterEach(() => {
+  //   cy.deleteAllIndices();
+  //   cy.deleteADSystemIndices();
+  // });
 
   it('Full creation - based on real index', () => {
     // Define detector step
