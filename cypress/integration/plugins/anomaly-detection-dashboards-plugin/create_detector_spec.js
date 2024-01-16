ba436fcc459fa7ed686005828cab5df5d7ba490e
Lin Wang <wonglam@amazon.com>
2024-01-16 16:55:55 +0800

    test: add visit for test
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- cypress/integration/plugins/anomaly-detection-dashboards-plugin/create_detector_spec.js
+++ cypress/integration/plugins/anomaly-detection-dashboards-plugin/create_detector_spec.js
@@ -3,8 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// import { AD_FIXTURE_BASE_PATH, AD_URL } from '../../../utils/constants';
-import { AD_FIXTURE_BASE_PATH } from '../../../utils/constants';
+import { AD_FIXTURE_BASE_PATH, AD_URL } from '../../../utils/constants';
 // import { selectTopItemFromFilter } from '../../../utils/helpers';
 
 context('Create detector workflow', () => {
@@ -43,6 +42,54 @@ context('Create detector workflow', () => {
   });
 
   it('Full creation - based on real index', () => {
-    console.log('empty test case...');
+    // Define detector step
+    cy.visit(AD_URL.CREATE_AD);
+    console.log('after visit create page');
+    // cy.getElementByTestId('defineOrEditDetectorTitle').should('exist');
+    // cy.getElementByTestId('detectorNameTextInput').type(TEST_DETECTOR_NAME);
+    // cy.getElementByTestId('detectorDescriptionTextInput').type(
+    //   TEST_DETECTOR_DESCRIPTION
+    // );
+    // cy.getElementByTestId('indicesFilter').type(`${TEST_INDEX_NAME}{enter}`);
+    // selectTopItemFromFilter('timestampFilter', false);
+
+    // cy.getElementByTestId('defineDetectorNextButton').click();
+    // cy.getElementByTestId('defineOrEditDetectorTitle').should('not.exist');
+    // cy.getElementByTestId('configureOrEditModelConfigurationTitle').should(
+    //   'exist'
+    // );
+
+    // // Configure model step
+    // cy.getElementByTestId('featureNameTextInput-0').type(TEST_FEATURE_NAME);
+    // selectTopItemFromFilter('featureFieldTextInput-0', false);
+    // cy.getElementByTestId('configureModelNextButton').click();
+    // cy.getElementByTestId('configureOrEditModelConfigurationTitle').should(
+    //   'not.exist'
+    // );
+    // cy.getElementByTestId('detectorJobsTitle').should('exist');
+
+    // // Set up detector jobs step
+    // cy.getElementByTestId('detectorJobsNextButton').click();
+    // cy.getElementByTestId('detectorJobsTitle').should('not.exist');
+    // cy.getElementByTestId('reviewAndCreateTitle').should('exist');
+
+    // // Review and create step
+    // cy.getElementByTestId('detectorNameCell').contains(TEST_DETECTOR_NAME);
+    // cy.getElementByTestId('detectorDescriptionCell').contains(
+    //   TEST_DETECTOR_DESCRIPTION
+    // );
+    // cy.getElementByTestId('indexNameCell').contains(TEST_INDEX_NAME);
+    // cy.getElementByTestId('timestampNameCell').contains(TEST_TIMESTAMP_NAME);
+    // cy.getElementByTestId('featureTable').contains(TEST_FEATURE_NAME);
+    // cy.getElementByTestId('createDetectorButton').click();
+
+    // // Wait 5s for the detector to be created and started. By default,
+    // // real-time detection will start.
+    // cy.wait(5000);
+
+    // // Lands on the config page by default.
+    // cy.getElementByTestId('detectorSettingsHeader').should('exist');
+    // cy.getElementByTestId('modelConfigurationHeader').should('exist');
+    // cy.getElementByTestId('detectorJobsHeader').should('exist');
   });
 });
