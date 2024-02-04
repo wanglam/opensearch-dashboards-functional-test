debb64de3f289793f9918c5eb83038d8e8d706f7
Lin Wang <wonglam@amazon.com>
2024-02-04 15:10:50 +0800

    remove tenant override
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/data_explorer/index_pattern_without_field.spec.js
+++ cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/data_explorer/index_pattern_without_field.spec.js
@@ -17,7 +17,6 @@ const testFixtureHandler = new TestFixtureHandler(
 
 describe('index pattern without field spec', () => {
   before(() => {
-    CURRENT_TENANT.newTenant = 'global';
     testFixtureHandler.importJSONMapping(
       'cypress/fixtures/dashboard/opensearch_dashboards/data_explorer/index_pattern_without_timefield/mappings.json.txt'
     );
@@ -48,56 +47,12 @@ describe('index pattern without field spec', () => {
   });
 
   it('should display a timepicker after switching to an index pattern with timefield', () => {
-    // cy.intercept('POST', '/api/saved_objects/_bulk_get').as(
-    //   'getIndexPatternDetail'
-    // );
     const indexName = 'with-timefield';
     cy.getElementByTestId('comboBoxToggleListButton')
       .should('be.visible')
       .click();
     cy.contains('button', indexName).click();
     cy.waitForLoader();
-    // cy.wait('@getIndexPatternDetail').then((req) => {
-    //   req.response.body.saved_objects.forEach((item) => {
-    //     if (item.error) {
-    //       Cypress.log({
-    //         displayName: 'error',
-    //         message: `[${item.id}]-${item.type}`,
-    //       });
-    //     }
-    //   });
-    // });
-    cy.request({
-      url: '/api/saved_objects/_bulk_get',
-      method: 'POST',
-      body: JSON.stringify([{ type: 'index-pattern', id: indexName }]),
-      headers: {
-        'content-type': 'application/json',
-        'osd-xsrf': true,
-      },
-    }).then(({ body }) => {
-      const savedObject = body.saved_objects[0];
-      if (
-        savedObject &&
-        savedObject.attributes &&
-        savedObject.attributes.timeFieldName
-      ) {
-        cy.log('find:' + JSON.stringify(savedObject.attributes.timeFieldName));
-      } else {
-        cy.log('fallback' + JSON.stringify(savedObject));
-      }
-    });
-
-    if (Cypress.env('SECURITY_ENABLED')) {
-      cy.request(`/api/v1/auth/dashboardsinfo`).then(({ body }) => {
-        cy.log(JSON.stringify(body));
-      });
-      cy.request(
-        `${Cypress.env('openSearchUrl')}/_plugins/_security/api/tenancy/config`
-      ).then(({ body }) => {
-        cy.log(JSON.stringify(body));
-      });
-    }
     cy.getElementByTestId('superDatePickerToggleQuickMenuButton').should(
       'be.visible'
     );
