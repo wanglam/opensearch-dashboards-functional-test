b83e68e072dc17967c7f01b24ca79dbdb6a048be
Lin Wang <wonglam@amazon.com>
2024-02-05 14:14:33 +0800

    clear not used code
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/data_explorer/index_pattern_without_field.spec.js
+++ cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/data_explorer/index_pattern_without_field.spec.js
@@ -31,121 +31,32 @@ describe('index pattern without field spec', () => {
     });
   });
 
-  beforeEach(() => {
-    cy.request({
-      url: '/api/saved_objects/_find?fields=title&per_page=10000&type=index-pattern',
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-        'osd-xsrf': true,
-      },
-    }).then(({ body }) => {
-      cy.log('find:' + JSON.stringify(body));
-    });
-    cy.wait(1000);
-    // Go to the Discover page
-    miscUtils.visitPage('app/data-explorer/discover#/');
-    cy.waitForLoader();
-    cy.request({
-      url: '/api/saved_objects/_find?fields=title&per_page=10000&type=index-pattern',
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-        'osd-xsrf': true,
-      },
-    }).then(({ body }) => {
-      cy.log('find:' + JSON.stringify(body));
-    });
-    cy.wait(1000);
-  });
-
   after(() => {
     testFixtureHandler.clearJSONMapping(
       'cypress/fixtures/dashboard/opensearch_dashboards/data_explorer/index_pattern_without_timefield/mappings.json.txt'
     );
   });
 
+  beforeEach(() => {
+    // Move visit page to before each to avoid test cases failed in security enabled
+    // Go to the Discover page
+    miscUtils.visitPage('app/data-explorer/discover#/');
+    cy.waitForLoader();
+  });
+
   it('should not display a timepicker', () => {
     cy.getElementByTestId('superDatePickerToggleQuickMenuButton').should(
       'not.exist'
     );
-    cy.request({
-      url: '/api/saved_objects/_find?fields=title&per_page=10000&type=index-pattern',
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-        'osd-xsrf': true,
-      },
-    }).then(({ body }) => {
-      cy.log('find:' + JSON.stringify(body));
-    });
-    cy.wait(2000);
   });
 
   it('should display a timepicker after switching to an index pattern with timefield', () => {
-    cy.request({
-      url: '/api/saved_objects/_find?fields=title&per_page=10000&type=index-pattern',
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-        'osd-xsrf': true,
-      },
-    }).then(({ body }) => {
-      cy.log('find before click:' + JSON.stringify(body));
-    });
-    cy.wait(2000);
-    // cy.intercept('POST', '/api/saved_objects/_bulk_get').as(
-    //   'getIndexPatternDetail'
-    // );
     const indexName = 'with-timefield';
     cy.getElementByTestId('comboBoxToggleListButton')
       .should('be.visible')
       .click();
     cy.contains('button', indexName).click();
     cy.waitForLoader();
-    // cy.wait('@getIndexPatternDetail').then((req) => {
-    //   req.response.body.saved_objects.forEach((item) => {
-    //     if (item.error) {
-    //       Cypress.log({
-    //         displayName: 'error',
-    //         message: `[${item.id}]-${item.type}`,
-    //       });
-    //     }
-    //   });
-    // });
-    cy.request({
-      url: '/api/saved_objects/_find?fields=title&per_page=10000&type=index-pattern',
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-        'osd-xsrf': true,
-      },
-    }).then(({ body }) => {
-      cy.log('find:' + JSON.stringify(body));
-    });
-    cy.wait(1000);
-    cy.request({
-      url: '/api/saved_objects/_bulk_get',
-      method: 'POST',
-      body: JSON.stringify([{ type: 'index-pattern', id: indexName }]),
-      headers: {
-        'content-type': 'application/json',
-        'osd-xsrf': true,
-      },
-    }).then(({ body }) => {
-      const savedObject = body.saved_objects[0];
-      if (
-        savedObject &&
-        savedObject.attributes &&
-        savedObject.attributes.timeFieldName
-      ) {
-        cy.log(
-          'bulk get:' + JSON.stringify(savedObject.attributes.timeFieldName)
-        );
-      } else {
-        cy.log('fallback' + JSON.stringify(savedObject));
-      }
-    });
     cy.getElementByTestId('superDatePickerToggleQuickMenuButton').should(
       'be.visible'
     );
