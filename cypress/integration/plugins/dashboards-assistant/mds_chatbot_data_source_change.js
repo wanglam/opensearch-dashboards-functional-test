fda11e8271fc6c12a301fdf8f95a19f3ac301d0c
Lin Wang <wonglam@amazon.com>
2024-06-07 16:01:51 +0800

    --wip-- [skip ci]

--- cypress/integration/plugins/dashboards-assistant/mds_chatbot_data_source_change.js
+++ cypress/integration/plugins/dashboards-assistant/mds_chatbot_data_source_change.js
@@ -2,7 +2,6 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-import { BASE_PATH } from '../../../utils/constants';
 
 const manualSetDefaultDataSource = (dataSourceTitle) => {
   // Goto data source detail page
@@ -14,6 +13,28 @@ const manualSetDefaultDataSource = (dataSourceTitle) => {
   }
   // Back to data source list
   cy.getElementByTestId('dataSources').click();
+};
+
+const openChatBotAndSendMessage = () => {
+  // Common text to wait for to confirm page loaded, give up to 120 seconds for initial load
+  cy.get(`input[placeholder="Ask question"]`, { timeout: 120000 }).as(
+    'chatInput'
+  );
+  cy.get('@chatInput').should('be.length', 1);
+
+  cy.wait(1000);
+
+  cy.get('@chatInput')
+    .click()
+    .type('What are the indices in my cluster?{enter}');
+
+  // should have a LLM Response
+  cy.contains(
+    'The indices in your cluster are the names listed in the response obtained from using a tool to get information about the OpenSearch indices.'
+  ).should('be.visible');
+
+  // Should have three bubbles
+  cy.get('[aria-label="chat message bubble"]').should('have.length', 3);
 };
 
 if (
@@ -44,7 +65,7 @@ if (
       manualSetDefaultDataSource(dataSource1.title);
     });
 
-    it('should reload history with new data source id', function () {
+    it('should reload history with new data source id', () => {
       // The header may render multiple times, wait for UI to be stable
       cy.wait(5000);
       // enable to toggle and show Chatbot
@@ -62,43 +83,59 @@ if (
       cy.wait('@loadConversationsRequest').then(({ request }) => {
         expect(request.url).contains(dataSource2.id);
       });
-      // return to chat tab
+      // Back to chat tab
       cy.get('.llm-chat-flyout button[aria-label="history"]')
         .should('be.visible')
         .click();
-      //close chat bot
+      // Close chat bot
+      cy.get(`img[aria-label="toggle chat flyout icon"]`).click();
+    });
+
+    it('should not reset to chat tab after data source change in history page', () => {
+      openChatBotAndSendMessage();
+
+      cy.get('.llm-chat-flyout button[aria-label="history"]')
+        .should('be.visible')
+        .click();
+
+      manualSetDefaultDataSource(dataSource2.title);
+
+      // Should reset conversation and stay history page
+      cy.get('h3').contains('OpenSearch Assistant').should('be.visible');
+      cy.get('h3').contains('Conversations').should('be.visible');
+
+      // Back to chat tab
+      cy.get('.llm-chat-flyout button[aria-label="history"]')
+        .should('be.visible')
+        .click();
+      // Close chat bot
       cy.get(`img[aria-label="toggle chat flyout icon"]`).click();
     });
 
     it('should reset chat conversation after data source changed', () => {
-      // Common text to wait for to confirm page loaded, give up to 120 seconds for initial load
-      cy.get(`input[placeholder="Ask question"]`, { timeout: 120000 }).as(
-        'chatInput'
-      );
-      cy.get('@chatInput').should('be.length', 1);
-
-      cy.wait(1000);
-
-      cy.get('@chatInput')
-        .click()
-        .type('What are the indices in my cluster?{enter}');
-
-      // should have a LLM Response
-      cy.contains(
-        'The indices in your cluster are the names listed in the response obtained from using a tool to get information about the OpenSearch indices.'
-      ).should('be.visible');
-
-      // Should have three bubbles
-      cy.get('[aria-label="chat message bubble"]').should('have.length', 3);
+      openChatBotAndSendMessage();
 
       manualSetDefaultDataSource(dataSource2.title);
 
       // Should reset chat
-      cy.get('h3').contains('OpenSearch Assistant');
+      cy.get('h3').contains('OpenSearch Assistant').should('be.visible');
       cy.get('[aria-label="chat message bubble"]').should('have.length', 1);
 
       //close chat bot
       cy.get(`img[aria-label="toggle chat flyout icon"]`).click();
+    });
+
+    it('should reset chat tab after data source changed in trace page', () => {
+      openChatBotAndSendMessage();
+      // click view trace button
+      cy.get(`[aria-label="How was this generated?"]`).click();
+      cy.contains('How was this generated').should('be.visible');
+
+      manualSetDefaultDataSource(dataSource2.title);
+
+      // Should reset chat tab
+      cy.get('#how-was-this-generated').should('not.exist');
+      cy.get('h3').contains('OpenSearch Assistant').should('be.visible');
     });
 
     after(() => {
